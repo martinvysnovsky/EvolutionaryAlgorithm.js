c0740b306e066a8f7d5a6184c72587c415259685
Martin Vyšňovský <martinvysnovsky@gmail.com>
2014-03-09 20:51:58 +0100

    Initial version of library. For now you can create population of individuals and select parents from them. There is several methods to parents selection.

---
+++ ea.js
@@ -0,0 +1,373 @@
+/**
+ * Copyright 2014 Martin Vyšňovský (martinvysnovsky@gmail.com)
+ */
+
+/**
+ * Evolutionary algorithm constructor.
+ *
+ * @param  array  variables  Variables for algorithm.
+ */
+function EA(variables)
+{
+	this.variables = [];
+
+	if(Object.prototype.toString.call(variables) === '[object Array]')
+	{
+		this.variables = this.variables.concat(variables);
+	}
+	else
+		this.variables.push(variables);
+}
+
+EA.prototype = {
+	constructor: EA,
+
+	/**
+	 * Initialize population from individuals.
+	 *
+	 * @param   string  method  Name of methot to use. Supported values: random
+	 * @param   int     n       Number of individuals.
+	 *
+	 * @return  array           Population.
+	 */
+	initializePopulation: function(method, n)
+	{
+		// defaults
+		method = method || 'random';
+		n = n || 0;
+
+		var population = new EAPopulation();
+
+		if(n > 0)
+		{
+			var generateIndividualFunction;
+			switch(method)
+			{
+				case 'random':
+				default:
+					generateIndividualFunction = function() { return (Math.random() * 2) - 1; };
+			}
+		
+			while(population.count < n)
+			{
+				var individual = new EAIndividual(this.variables, generateIndividualFunction);
+
+				//if(population.hasIndividual(individual))
+				//	continue;
+
+				population.push(individual);
+			}
+		}
+
+		return population;
+	}
+};
+
+/**
+ * Individiual for evolutionary algorithm.
+ *
+ * @param  array  variables  Array of variable names.
+ * @param  array  f  		 Function that generates individual value.
+ */
+function EAIndividual(variables, f)
+{
+	if(variables)
+	{
+		for(var v=0, len=variables.length; v<len; v++)
+		{
+			this[variables[v]] = f();
+		}
+	}
+
+	this.fitness = 0;
+}
+
+EAIndividual.prototype = {
+	constructor: EAIndividual,
+	toString: function()
+	{
+		ret = new Array();
+
+		for(p in this)
+		{
+			if(this.hasOwnProperty(p))
+				ret.push(p + ': ' + this[p]);
+		}
+
+		return ret.join(', ');
+	}
+}
+
+/**
+ * Population of individuals.
+ *
+ * @param  array  individuals  Array of individuals in population.
+ */
+function EAPopulation()
+{
+	this.individuals = new Array();
+}
+
+EAPopulation.prototype = (function()
+{
+	/**
+	 * Get n random individuals in population.
+	 *
+	 * @param   int    n  Number of individuals.
+	 *
+	 * @return  array     Array of individuals.
+	 */
+	function getRandomParents(n)
+	{
+		var individuals = new Array(n);
+		var individuals_length = this.individuals.length;
+
+		for(var i=0; i<n; i++)
+		{
+			var index = Math.floor(Math.random() * individuals_length);
+
+			individuals[i] = this.individuals[index];
+		}
+
+		return individuals;
+	}
+
+	/**
+	 * Get n best individuals in population.
+	 *
+	 * @param   int    n  Number of individuals.
+	 *
+	 * @return  array     Array of best individuals.
+	 */
+	function getBestParents(n)
+	{
+		var individuals = this.individuals.slice(0);
+
+		individuals.sort(function(a, b) {
+			return b.fitness - a.fitness;
+		});
+
+		return individuals.slice(0, n);
+	}
+
+	/**
+	 * Get n individuals from population using roulette method.
+	 *
+	 * @param   string  method  	  Method to use.
+	 * @param   int     n  			  Number of individuals.
+	 * @param   bool    shuffleOrder  Shuffle order of individuals. This is used only for univerzal method.
+	 *
+	 * @return  array       	Array of individuals.
+	 */
+	function getParentsFromRoulette(method, n, shuffleOrder)
+	{
+		var individuals   = this.individuals;
+		var fitnessValues = individuals.slice(0).map(function(individual)
+		{
+			return individual.fitness;
+		});
+		var parents       = new Array(n);
+		var i             = 0; // parent counter
+		var rouletteSize  = 0;
+		var replacement   = function() { };
+
+		switch(method)
+		{
+			case 'with_replacement':
+			case 'without_replacement':
+			default:
+				// compute size of roulette
+				rouletteSize = individuals.reduce(function(a, b)
+				{
+					return {fitness: a.fitness + Math.max(0, b.fitness)};
+				}, {fitness: 0}).fitness;
+				break;
+			case 'remainder_with_replacement':
+			case 'remainder_without_replacement':
+				individuals.forEach(function(individual)
+				{
+					// get only whole part of fitness
+					var wholePart = Math.floor(Math.max(0, individual.fitness));
+
+					for(var j=0; j<wholePart; j++, i++)
+						parents[i] = individual;
+				});
+
+				// compute size of roulette only from decimal part of fitness
+				rouletteSize = individuals.reduce(function(a, b)
+				{
+					return {fitness: a.fitness + Math.max(0, b.fitness) % 1};
+				}, {fitness: 0}).fitness;
+
+				fitnessValues = fitnessValues.map(function(fitness)
+				{
+					return (Math.max(0, fitness) % 1);
+				});
+				break;
+			case 'univerzal':
+				var shuffleOrder = shuffleOrder || false;
+				var keys         = Object.keys(individuals);
+
+				if(shuffleOrder)
+				{
+
+					// randomly shuffle order of individuals
+					for(var a=0, len=keys.length; a<len; a++)
+					{
+						b = a + Math.round(Math.random() * (len - a - 1));
+						var temp = keys[a];
+						keys[a] = keys[b];
+						keys[b] = temp;
+					}
+				}
+
+				// compute size of roulette
+				rouletteSize = individuals.reduce(function(a, b)
+				{
+					return {fitness: a.fitness + Math.max(0, b.fitness)};
+				}, {fitness: 0}).fitness;
+
+				var pointerStep = rouletteSize / n;
+
+				// compute start position
+				var roulette_position = Math.random() * pointerStep;
+
+				f = 0;
+				j = 0;
+
+				// select n parents
+				for(i; i<n; i++)
+				{
+					// find parent that is at computed position in roulette
+					while(f < roulette_position)
+						f += Math.max(0, fitnessValues[keys[j++]]);
+					
+					parents[i] = individuals[keys[j-1]];
+
+					roulette_position += pointerStep;
+				}
+
+				return parents;
+		}
+
+		if(method == 'without_replacement' || method == 'remainder_without_replacement')
+		{
+			replacement = function(j)
+			{
+				rouletteSize--;
+				fitnessValues[j] = Math.max(0, fitnessValues[j]-1);
+			};
+		}
+
+		// select n parents
+		for(i; i<n; i++)
+		{
+			// twist roulette
+			var roulette_position = Math.random() * rouletteSize;
+
+			// find parent that is at computed position in roulette
+			f = 0;
+			j = 0;
+			while(f < roulette_position)
+				f += fitnessValues[j++];
+			
+			parents[i] = individuals[j-1];
+
+			// call replacement function
+			replacement(j);
+		}
+
+		return parents;
+	}
+
+	return {
+		constructor: EAPopulation,
+
+		/**
+		 * Count individuals in population.
+		 *
+		 * @return  int
+		 */
+		get count()
+		{
+			return this.individuals.length;
+		},
+
+		/**
+		 * Insert individual to population.
+		 *
+		 * @param   object  individual
+		 *
+		 * @return  void
+		 */
+		push: function(individual)
+		{
+			if(!(individual instanceof EAIndividual))
+				throw new Error('Population must consists only from EAIndividual objects.');
+
+			this.individuals.push(individual);
+		},
+
+		/**
+		 * Check if population has given individual.
+		 *
+		 * @param   object   individual
+		 *
+		 * @return  boolean
+		 */
+		hasIndividual: function(individual)
+		{
+			return (this.individuals.indexOf(individual) != -1);
+		},
+
+		/**
+		 * Compute fitness for every individual in population.
+		 *
+		 * @param   function  f  Function, that will compute fitness.
+		 *
+		 * @return  void
+		 */
+		computeFitness: function(f)
+		{
+			if(typeof f != 'function')
+				throw new Error('Argument must be valid function!');
+
+			for(var i=0, len=this.individuals.length; i<len; i++)
+			{
+				var individual = this.individuals[i];
+			
+				individual.fitness = f(individual);
+			}
+		},
+
+		/**
+		 * Get n best individuals in population.
+		 *
+		 * @param   string  method   Method to use.
+		 * @param   int     n  		 Number of individuals.
+		 * @param   object  options  Some other settings for methods.
+		 *
+		 * @return  array      Array of best individuals.
+		 */
+		getParents: function(method, n, options)
+		{
+			switch(method)
+			{
+				case 'best':
+					return getBestParents.call(this, n);
+					break;
+				case 'roulette':
+					var rouletteMethod = options.rouletteMethod || 'with_replacement';
+					return getParentsFromRoulette.call(this, rouletteMethod, n, options.shuffleOrder);
+					break;
+				case 'random':
+					return getRandomParents.call(this, n);
+				default:
+
+			}
+
+			return [];
+		}
+	}
+
+}());
